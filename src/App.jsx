7f87c4f6848da7a5268c99d5e6d8962fe547a68c
Reza <rezammdzd0@gmail.com>
2023-09-03 19:13:11 +0330

    add note component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import "./App.css";
+import AddNewNote from "./components/AddNewNote";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+    return (
+        <div className="container">
+            <div className="note-header">note header</div>
+            <div className="note-app">
+                <AddNewNote />
+                <div className="note-container">nots</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
