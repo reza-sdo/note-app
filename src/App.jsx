621baa89ce81cb39af9990ebbbb2fc05eb4bf7bf
Reza <rezammdzd0@gmail.com>
2023-09-09 17:23:49 +0330

    handel completed note

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,29 @@ function App() {
         setNotes((prevN) => prevN.filter((n) => n.id !== id));
     };
 
+    const handelCompleted = (e) => {
+        // const completedNote = notes.map((n) =>
+        //     n.id === +e.target.id ? { ...n, completed: !n.completed } : n
+        // );
+        // setNotes(completedNote);
+
+        setNotes((prevN) =>
+            notes.map((n) =>
+                n.id === +e.target.id ? { ...n, completed: !n.completed } : n
+            )
+        );
+    };
     return (
         <div className="container">
             <div className="note-header">note header</div>
             <div className="note-app">
                 <AddNewNote onHandelNotes={handelNote} />
                 <div className="note-container">
-                    <NoteList notes={notes} onDelete={handelDeleteNote} />
+                    <NoteList
+                        notes={notes}
+                        onDelete={handelDeleteNote}
+                        onCompleted={handelCompleted}
+                    />
                 </div>
             </div>
         </div>
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 
-function NoteList({ notes  , onDelete}) {
+function NoteList({ notes, onDelete, onCompleted }) {
     return (
         <div className="note-list">
             {notes.map((note) => (
-                <NoteItem key={note.id} note={note} onDelete={onDelete} />
+                <NoteItem
+                    key={note.id}
+                    note={note}
+                    onDelete={onDelete}
+                    onCompleted={onCompleted}
+                />
             ))}
         </div>
     );
 }
 
-function NoteItem({ note, onDelete }) {
+function NoteItem({ note, onDelete ,onCompleted}) {
     const options = {
         year: "numeric",
         month: "short",
         day: "numeric",
     };
 
+    console.log(note);
+
     return (
-        <div className="note-item">
+        <div className={`note-item ${note.completed ? "completed" :"" }`}>
             <div className="note-item__header">
                 <div>
                     <p className="title">{note.title}</p>
                     <p className="desc">{note.description}</p>
                 </div>
                 <div className="actions">
-                    <button onClick={()=>onDelete(note.id)}>❌</button>
-                    <input type="checkbox" name="" id="" />
+                    <button onClick={() => onDelete(note.id)}>❌</button>
+                    <input
+                        type="checkbox"
+                        name=""
+                        id={note.id}
+                        // onChange={() => onCompleted(note.id)}
+                        onChange={onCompleted}
+
+                    />
                 </div>
             </div>
             <div className="note-item__footer" dir="rtl">
